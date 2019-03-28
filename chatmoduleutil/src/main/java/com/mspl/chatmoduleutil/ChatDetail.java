d7013304a24cb062bb6cf79e77a3e6746e91bb13
ashupa <apashutosh93@gmail.com>
2019-03-28 13:05:42 +0530

    todo for first commit

---
+++ chatmoduleutil/src/main/java/com/mspl/chatmoduleutil/ChatDetail.java
@@ -0,0 +1,13 @@
+package com.mspl.chatmoduleutil;
+
+import android.util.Log;
+
+/**
+ * Created by AgroNxt on 3/28/2019.
+ */
+
+public class ChatDetail {
+    public static void Chat(String message){
+        Log.e("hello",message);
+    }
+}
